29fd6e8345510c5a9d77a6dd9db2f5a76afd5a84
n-kats <n-kats19890214@hotmail.co.jp>
2017-10-15 01:13:56 +0900

    学習コマンドとモデルをつなげる部分の作成

--- actions/train.py
+++ actions/train.py
@@ -1,2 +1,33 @@
+from model.doc2vec_common import Doc2VecModel
+from preprocess import (
+    TeXMixedTextPreprocessor,
+    SimplePreprocessor,
+    AbsPreprocessor)
+
+
 def run(args):
-  print(args)
+  preprocesser = get_preprocessor(args.preprocess, args)
+  model = get_model(args.model, preprocesser, args)
+  train_docs = get_train_docs(args)
+  model.train(train_docs)
+  model.save(args.save_model)
+
+
+def get_train_docs(args):
+  return open(args.train_data)
+
+
+def get_preprocessor(name, args) -> AbsPreprocessor:
+  if name == "tex":
+    return TeXMixedTextPreprocessor()
+  elif name == "simple":
+    return SimplePreprocessor()
+  else:
+    raise NotImplementedError()
+
+
+def get_model(name, preprocesser, args):
+  if name == "doc2vec":
+    return Doc2VecModel(preprocesser)
+  else:
+    raise NotImplementedError()
